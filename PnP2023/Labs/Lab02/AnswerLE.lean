505547c10018d4094eeec2be9b6b1836473e3162
PatternRandomManas <manaskaushikdas@gmail.com>
2023-02-09 17:05:58 +0530

    lab2 commit

--- PnP2023/Labs/Lab02/AnswerLE.lean
+++ PnP2023/Labs/Lab02/AnswerLE.lean
@@ -3,4 +3,16 @@ import PnP2023.Lec_01_25.Answer
 namespace Waffle
 
 theorem Answer.eq_of_le_le (a b  : Answer) : 
-  a ≤ b → b ≤ a → a = b := by  sorry
+  a ≤ b → b ≤ a → a = b := 
+  by
+    intro h₁ h₂
+    cases c₁:h₁
+    case le_yes => 
+      cases c₂:h₂
+      case le_yes => rfl
+      case refl => rfl
+    case no_le => 
+      cases c₂:h₂
+      case refl => rfl 
+      case no_le => rfl      
+    case refl => rfl 
